2dc09ca87addd4cacd3cf10698dc5469bed30fbb
Rim-chan <rim.sleimi@eudiant-fst.utm.tn>
2022-08-22 17:13:27 +0200

    second commit

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ if __name__ == "__main__":
     callbacks = []
 
     compute_TCI = GetIndices(args, index='TCI', sum=False).get_scaled_index()
+    print('TCI dataset:'+ str(compute_TCI.size().getInfo()))
+
     compute_VCI = GetIndices(args, index='VCI', sum=False).get_scaled_index()
+    print('VCI dataset:'+ str(compute_VCI.size().getInfo()))
+
     compute_ETCI = GetIndices(args, index='ETCI', sum=True).get_scaled_index()
+    print('ETCI dataset:'+ str(compute_ETCI.size().getInfo()))
+
     compute_PCI  = GetIndices(args, index='PCI', sum=True).get_scaled_index()
+    print('PCI dataset:'+ str(compute_PCI.size().getInfo()))
+
     compute_SMCI = GetIndices(args, index='SMCI', sum=False).get_scaled_index()
+    print('SMCI dataset:'+ str(compute_SMCI.size().getInfo()))
 
     

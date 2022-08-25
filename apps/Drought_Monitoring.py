3c119ecf754365fe45a6a7d64166dfd54b217c51
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-25 17:51:39 +0200

    Update Drought_Monitoring.py

--- apps/Drought_Monitoring.py
+++ apps/Drought_Monitoring.py
@@ -19,8 +19,8 @@ def app():
     # import the necessary libraries
     import ee
     import geemap.foliumap as geemap
-    import gc
-    import numpy as np
+#     import gc
+#     import numpy as np
     from dataset import GetIndices
 #     from pca import getPrincipalComponents
 #     from CMDI import compute_CMDI
@@ -127,7 +127,7 @@ def app():
 
             
     ## PCA
-    st.subheader('Compute CMDI')
+#     st.subheader('Compute CMDI')
     
 #     gc.collect()
 #     Image = ee.Image.cat([VCI_image.clip(roi), 

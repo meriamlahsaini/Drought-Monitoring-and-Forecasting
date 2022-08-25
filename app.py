f5b13c6e852d526703b5feb4c131623aaa0196b2
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-25 14:37:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home       #, basemaps, customize, datasets, opacity, nlcd_demo
+from apps import home, Drought_Monitoring
 from dataset import *
 from args import get_main_args
 
@@ -12,7 +12,7 @@ apps = MultiApp()
 # Add all your application here
 
 apps.add_app("Home", home.app)
-# apps.add_app("Customize the default map", customize.app)
+apps.add_app("Drought Monitoring", Drought_Monitoring.app)
 # apps.add_app("Change basemaps", basemaps.app)
 # apps.add_app("Change opacity", opacity.app)
 # apps.add_app("Search datasets", datasets.app)

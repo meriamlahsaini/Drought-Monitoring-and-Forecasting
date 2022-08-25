61e42b10740edbcab4008d4ceaeb5f9ec17fb1ae
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-25 14:44:32 +0200

    Update Drought_Forecasting.py

--- apps/Drought_Forecasting.py
+++ apps/Drought_Forecasting.py
@@ -1,1 +1,16 @@
+import streamlit as st
 
+
+def app():
+    st.title("Drought Forecasting")
+
+    countries = (
+        "Zambia",
+        "Ethiopia"
+    )
+
+    country = st.selectbox("Country", countries)
+   
+
+
+    st.header("Example")

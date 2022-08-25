d0868e463e7e0d4a4758e8e095f2ad363eb69872
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-25 14:56:41 +0200

    Update Drought_Forecasting.py

--- apps/Drought_Forecasting.py
+++ apps/Drought_Forecasting.py
@@ -10,6 +10,10 @@ def app():
     )
 
     country = st.selectbox("Country", countries)
+    
+    study_years = st.slider("Study Years", 2012, 2022, 2012)
+    study_months_gs = st.slider("Study Months", 1, 4, 1)   # growing season
+    study_months_ss = st.slider("Study Months", 11, 12, 11)   # sowing season
    
 
 

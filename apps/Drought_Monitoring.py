6a64e0ebe23e8faa90821d47ac69916badeb769c
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-25 15:29:02 +0200

    Update Drought_Monitoring.py

--- apps/Drought_Monitoring.py
+++ apps/Drought_Monitoring.py
@@ -5,8 +5,12 @@ def app():
     st.title("Drought Monitoring")
 
     countries = (
+        "Afghanistan",
+        "Burkina Faso",
         "Ethiopia",
+        "Ghana",
         "Kenya",
+        "Senegal",
         "Zambia"
     )
 

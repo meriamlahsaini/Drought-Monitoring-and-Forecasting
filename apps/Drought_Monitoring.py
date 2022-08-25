3d5eb17ba1f636821ba65ffbf816bf9eb085eacb
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-25 14:44:08 +0200

    Update Drought_Monitoring.py

--- apps/Drought_Monitoring.py
+++ apps/Drought_Monitoring.py
@@ -10,18 +10,7 @@ def app():
     )
 
     country = st.selectbox("Country", countries)
-    
-    
-    
-    st.markdown(
-        """
-    This site demostrates how to build a multi-page [Earth Engine](https://earthengine.google.com) App using [streamlit](https://streamlit.io) and [geemap](https://geemap.org).
-    You can deploy the app on various cloud platforms, such as [share.streamlit.io](https://share.streamlit.io) or [Heroku](https://heroku.com).
-    Make sure you set `EARTHENGINE_TOKEN='your-token'` as an environment variable (secret) on the cloud platform.
-    - **Web App:** <https://gishub.org/geemap-apps>
-    - **Github:** <https://github.com/Rim-chan/geemap-apps>
-    """
-    )
+   
 
 
     st.header("Example")

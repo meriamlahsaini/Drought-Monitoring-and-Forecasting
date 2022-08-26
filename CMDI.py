c2ca2e567dc162ab58bbcff2ec8ac3e1d1bf1e5a
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-26 10:42:35 +0200

    Update CMDI.py

--- CMDI.py
+++ CMDI.py
@@ -1,10 +1,10 @@
 
 
-# def compute_CMDI(VCI, TCI, PCI, ETCI, SMCI, weights, roi):
-#     wVCI = VCI.clip(roi).multiply(weights[0])
-#     wTCI = TCI.clip(roi).multiply(weights[1]) 
-#     wPCI = PCI.clip(roi).multiply(weights[2])
-#     wETCI = ETCI.clip(roi).multiply(weights[3]) 
-#     wSMCI = SMCI.clip(roi).multiply(weights[4])  
+def compute_CMDI(VCI, TCI, PCI, ETCI, SMCI, weights, roi):
+    wVCI = VCI.clip(roi).multiply(weights[0])
+    wTCI = TCI.clip(roi).multiply(weights[1]) 
+    wPCI = PCI.clip(roi).multiply(weights[2])
+    wETCI = ETCI.clip(roi).multiply(weights[3]) 
+    wSMCI = SMCI.clip(roi).multiply(weights[4])  
  
-#     return  wVCI.add(wTCI).add(wPCI).add(wETCI).add(wSMCI)
+    return  wVCI.add(wTCI).add(wPCI).add(wETCI).add(wSMCI)

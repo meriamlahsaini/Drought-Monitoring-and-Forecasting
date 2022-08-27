b2b7ae6c593c8c9614be3caf09e4698f46e06f0a
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-27 08:31:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, Drought_Monitoring, Drought_Forecasting
+from apps import Introduction, Drought_Monitoring, Drought_Forecasting
 
 st.set_page_config(layout="wide")
 
@@ -9,12 +9,10 @@ apps = MultiApp()
 
 # Add all your application here
 
-apps.add_app("Home", home.app)
+apps.add_app("Introduction", Introduction.app)
 apps.add_app("Drought Monitoring", Drought_Monitoring.app)
 apps.add_app("Drought Forecasting", Drought_Forecasting.app)
-# apps.add_app("Change opacity", opacity.app)
-# apps.add_app("Search datasets", datasets.app)
-# apps.add_app("NLCD Demo", nlcd_demo.app)
+
 
 # The main app
 apps.run()

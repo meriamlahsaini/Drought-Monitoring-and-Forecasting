6eccf79a3fd0774637a8e4e51a27b559e81f9a38
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-08-27 08:36:22 +0200

    Update Introduction.py

--- apps/Introduction.py
+++ apps/Introduction.py
@@ -3,20 +3,30 @@ import streamlit as st
 
 def app():
     st.title("Introduction")
+    
+    st.sidebar.title("About")
+    st.sidebar.info(
+    """
+    Web App URL: <https://rim-chan-dm-app-344y39.streamlitapp.com/>
+    GitHub repository: <https://github.com/Rim-chan/DM>
+    """
+    )
+
+    st.sidebar.title("Contact")
+    st.sidebar.info(
+        """
+        [GitHub](https://github.com/Rim-chan) |[Gmail](rim.sleimi@etudiant-fst.utm.tn) | [LinkedIn](https://www.linkedin.com/in/rim-sleimi/)
+        """
+    )
+    
 
     st.markdown(
         """
     This multi-page web app demonstrates drought monitoring an forecasting created using [streamlit](https://streamlit.io) and open-source mapping libraries, 
     such as [leafmap](https://leafmap.org), [geemap](https://geemap.org), [pydeck](https://deckgl.readthedocs.io), and [kepler.gl](https://docs.kepler.gl/docs/keplergl-jupyter).
-    This is an open-source project and you are very welcome to contribute your comments, questions, resources, and apps as [issues](https://github.com/giswqs/streamlit-geospatial/issues) or 
-    [pull requests](https://github.com/giswqs/streamlit-geospatial/pulls) to the [GitHub repository](https://github.com/giswqs/streamlit-geospatial).
-
-    - **Web App:** <https://rim-chan-dm-app-344y39.streamlitapp.com/>
-    - **Github:** <https://github.com/Rim-chan/DM>
     """
     )
    
-    st.header('Introduction')
     st.markdown(
         """
     Drought is a climate-related disaster that negatively impacts a variety of sectors and poses challenges for millions of people especially in African countries.

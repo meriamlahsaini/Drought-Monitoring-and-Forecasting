fa762f4090ffd08f9e338cbf6ecb23e8dc3b4b8d
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-09-25 19:22:29 +0200

    Update Drought_Monitoring.py

--- apps/Drought_Monitoring.py
+++ apps/Drought_Monitoring.py
@@ -33,11 +33,6 @@ def app():
     st.subheader("Define ROI")
     roi_st = time.time()
     roi_bar = st.progress(0)
-#     roi_bar = st.progress(time.time())
-    
-    for percent_complete in range(100):
-        time.sleep(0.1)
-        roi_bar.progress(percent_complete + 1)
     
     country = st.selectbox("Country", countries)
     if country == "Afghanistan":
@@ -62,9 +57,15 @@ def app():
         Map.addLayer(roi, {}, country +'Boundary Map') 
         Map.to_streamlit()
 
-#     roi_et = time.time()
-#     roi_time = roi_et - roi_st
+    roi_et = time.time()
+    roi_time = roi_et - roi_st
+    roi_bar = st.progress(time.time())
     
+#     for percent_complete in range(100):
+#         time.sleep(0.1)
+#         roi_bar.progress(percent_complete + 1)
+        
+        
     ## INPUT INDICES: VCI, TCI, PCI, ETCI, SMCI
     st.subheader('Compute Input Indices')
     

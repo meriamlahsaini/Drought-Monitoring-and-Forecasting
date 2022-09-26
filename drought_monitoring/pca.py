ae9820fc07a16d818849dc2db7ccce5bf4b64abf
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-09-26 23:10:42 +0200

    Update pca.py

--- drought_monitoring/pca.py
+++ drought_monitoring/pca.py
@@ -1,8 +1,6 @@
 import ee
-import streamlit as st
 
 # This helper function returns a list of new band names.
-@st.cache
 def getNewBandNames(prefix, bandNames):
   seq = ee.List.sequence(1, len(bandNames))
   return seq.map(lambda b: ee.String(prefix).cat(ee.Number(b).int().format()))

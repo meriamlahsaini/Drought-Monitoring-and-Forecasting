fc5924d613d2a31dd1d95cf2f2032004b5fbcea8
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-09-26 23:00:36 +0200

    Update CMDI.py

--- drought_monitoring/CMDI.py
+++ drought_monitoring/CMDI.py
@@ -1,5 +1,6 @@
+import streamlit as st
 
-
+@st.cache
 def compute_CMDI(VCI, TCI, PCI, ETCI, SMCI, weights, roi):
     wVCI = VCI.clip(roi).multiply(weights[0])
     wTCI = TCI.clip(roi).multiply(weights[1]) 

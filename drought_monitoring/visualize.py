f4bfa767c19beed15db41343f82cbd9ed9b4b02e
Rim Sleimi <64995617+Rim-chan@users.noreply.github.com>
2022-09-27 13:54:42 +0100

    Update and rename map_index.py to visualize.py

---
+++ drought_monitoring/visualize.py
@@ -0,0 +1,22 @@
+# import ee, geemap
+# geemap.ee_initialize()
+  
+# import streamlit as st
+  
+  
+# @st.cache
+# def map_index(image, roi, index):
+#   if index ==
+  
+  
+  
+  
+  
+  
+  
+#   Map = geemap.Map(zoom = 6, plugin_Draw=True, Draw_export=False)
+#   Map.centerObject(roi, 6)
+  
+#   Map.addLayer(image.clip(roi), args.tciVis, 'TCI, ' + d.strftime("%B %Y")) 
+#   Map.add_colorbar(args.tciVis, label="TCI", orientation="vertical", layer_name="TCI, " + d.strftime("%B %Y"))
+#   Map.to_streamlit()
